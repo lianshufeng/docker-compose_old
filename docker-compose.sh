d533894eb7e48a21a0c15811e859905185810a5a
lianshufeng <251708339@qq.com>
2020-11-13 14:35:40 +0800

    取消指定容器的名称

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 if [[ $(uname -m) == "x86_64" ]];then tag="latest"; else tag=$(uname -m);fi
-docker run --name docker-compose --rm -t -i -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):$(pwd) -w $(pwd) lianshufeng/docker-compose:$tag "$@"
+docker run --rm -t -i -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):$(pwd) -w $(pwd) lianshufeng/docker-compose:$tag "$@"
